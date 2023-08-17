bc95597c8193f5532f840d98a03fc088674a1cc8
motojate <motojate@naver.com>
2023-08-17 20:57:53 +0900

    * 버튼 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import '@fontsource/roboto/300.css'
+import BasicButtons from './components/Button'
 import logo from './logo.svg'
 import './App.css'
 
@@ -19,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        {BasicButtons()}
       </header>
     </div>
   )
