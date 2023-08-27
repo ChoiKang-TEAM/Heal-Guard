2c899dc1f872eb4b6f7fd98a4ec2650b43f24bec
motojate <motojate@naver.com>
2023-08-27 23:54:42 +0900

    * Input Basic Story push

--- client/src/components/inputs/Input.stories.tsx
+++ client/src/components/inputs/Input.stories.tsx
@@ -20,29 +20,9 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const Primary: Story = {
+export const InputByText: Story = {
   args: {
-    primary: true,
-    label: 'Button',
-  },
-}
-
-export const Secondary: Story = {
-  args: {
-    label: 'Button',
-  },
-}
-
-export const Large: Story = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-}
-
-export const Small: Story = {
-  args: {
-    size: 'small',
-    label: 'Button',
+    type: 'text',
+    label: 'InputByText',
   },
 }
