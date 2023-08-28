2d2a5b30f8dcb812944c4b292fe9d231499abf7f
motojate <motojate@naver.com>
2023-08-28 20:39:13 +0900

    * 리액트 라우터 관련 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,38 +1,11 @@
-import React, { useState } from 'react'
-import { ButtonByMui } from './components/buttons/Button'
-import { InputByMui } from './components/inputs/Input'
-import { handleInputChange } from './utils/handlers/InputChangeHandler'
-import './App.css'
+import React from 'react'
+import { Route } from 'react-router-dom'
+import { Login } from 'pages/sign-up/Login'
 
 function App() {
-  const [fields, setFields] = useState<{ [key: string]: string }>({
-    userId: '',
-    password: '',
-  })
-
-  const onLogin = () => {
-    console.log(fields)
-  }
   return (
     <>
-      <InputByMui
-        value={fields.userId}
-        type="text"
-        label="아이디"
-        onChange={(e) => handleInputChange(e, 'userId', fields, setFields)}
-      />
-      <InputByMui
-        value={fields.password}
-        type="password"
-        label="비밀번호"
-        onChange={(e) => handleInputChange(e, 'password', fields, setFields)}
-      />
-      <ButtonByMui
-        color="primary"
-        variant="outlined"
-        label="로그인"
-        onClick={onLogin}
-      />
+      <Route path="/login" Component={Login} />
     </>
   )
 }
