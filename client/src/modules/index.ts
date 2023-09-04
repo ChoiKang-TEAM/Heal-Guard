0adf707fc1dcc215d50b8998185b5d3207bb6edb
motojate <motojate@naver.com>
2023-09-04 14:29:33 +0900

    * 라우터 및 리덕스 인덱스 수정 및 추가

--- client/src/modules/index.ts
+++ client/src/modules/index.ts
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux'
-import userReducer from './user/userReducer'
+import userSlice from './user/userSlice'
 
 const rootReducer = combineReducers({
-  userReducer,
+  userReducer: userSlice.reducer,
 })
 
 export default rootReducer
--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Login from 'pages/sign-up/Login'
 import Home from 'pages/Home'
+import SignUp from 'pages/sign-up/SignUp'
 
 const RouterConfig = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/sign-up" element={<SignUp />} />
     </Routes>
   )
 }
