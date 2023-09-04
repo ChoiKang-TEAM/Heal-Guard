8bcbcde730c0f1bc1e1490f389d0f83cf2d5840a
motojate <motojate@naver.com>
2023-09-04 14:28:31 +0900

    * 리턴타입 정의

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,6 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import rootReducer from 'modules/index'
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 
 const store = configureStore({ reducer: rootReducer })
 
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<
+  ReturnType<typeof store.getState>
+> = useSelector
 export default store
