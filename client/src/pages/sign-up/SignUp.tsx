4cf6a8953a7526a64c68bc0230c23e6712fddac3
motojate <motojate@naver.com>
2023-09-04 14:29:39 +0900

    * 회원가입 페이지 생성

--- client/src/pages/sign-up/SignUp.tsx
+++ client/src/pages/sign-up/SignUp.tsx
@@ -1,1 +1,27 @@
-export default {}
+import { User } from 'common/interfaces/users/user'
+import { ButtonByMui } from 'components/buttons/Button'
+import React from 'react'
+import { loginUser } from 'modules/user/userAction'
+import { useAppDispatch, useAppSelector } from 'store'
+
+const SingUp = () => {
+  const user: User = useAppSelector((state) => state.userReducer.user)
+  const status = useAppSelector((state) => state.userReducer.status)
+  const dispatch = useAppDispatch()
+  const login = (mode: boolean) => {
+    if (mode) dispatch(loginUser({ userId: 'test', password: 'qwer1234!!' }))
+    else dispatch(loginUser({ userId: 'sandy-babo', password: 'qwer1234!!' }))
+  }
+  return (
+    <>
+      {user.userId} | {status}
+      {user.status}
+      <br />
+      <ButtonByMui label="성공 버튼" onClick={() => login(true)} />
+      <br />
+      <ButtonByMui label="실패 버튼" onClick={() => login(false)} />
+    </>
+  )
+}
+
+export default SingUp
