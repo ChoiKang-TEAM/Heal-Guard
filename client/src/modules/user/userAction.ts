17bece196f8b362f000cfe74160d014fd2964e57
motojate <motojate@naver.com>
2023-09-04 14:29:20 +0900

    * 유저 관련 리덕스 변경

--- client/src/modules/user/userAction.ts
+++ client/src/modules/user/userAction.ts
@@ -1,49 +1,13 @@
 import { getUser } from 'apis/get-user'
 import { User } from 'common/interfaces/users/user'
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { createAsyncThunk } from '@reduxjs/toolkit'
 
 export const loginUser = createAsyncThunk(
   'users/fetch',
   async (loginDto: User) => {
-    const response = await getUser(loginDto)
-    return response
+    const res = await getUser(loginDto)
+    const data = { user: { ...loginDto }, token: res }
+
+    return data
   }
 )
-
-const initialState: User = {
-  userId: '',
-  BUDI: '',
-  gender: '',
-  password: '',
-}
-
-const decode = (token: string): User => {
-  token
-  return {
-    userId: 'as',
-    password: 'as',
-  }
-}
-
-const userSlice = createSlice({
-  name: 'user',
-  initialState: { user: initialState, status: 'idle', error: null },
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(loginUser.pending, (state) => {
-        state.status = 'loading'
-      })
-      .addCase(loginUser.fulfilled, (state, action) => {
-        state.status = 'complete'
-
-        state.user = decode(action.payload || '')
-      })
-      .addCase(loginUser.rejected, (state, action) => {
-        state.status = 'failed'
-        state.error = action.error.message
-      })
-  },
-})
-
-export default userSlice.reducer
---
+++ client/src/modules/user/userSlice.ts
@@ -0,0 +1,45 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+import { loginUser } from './userAction'
+import { User } from 'common/interfaces/users/user'
+
+const initialState: User = {
+  userId: '',
+  password: '',
+}
+
+const setTokenHelper = (token: string) => {
+  console.log(1)
+  localStorage.setItem('token', token)
+}
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState: { user: initialState, status: 'idle', error: null },
+  reducers: {
+    setToken: (state, action: PayloadAction<{ token: string }>): void => {
+      setTokenHelper(action.payload.token)
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(loginUser.pending, (state) => {
+        state.status = 'loading'
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.status = 'complete'
+        if (action.payload.token) {
+          state.user = action.payload.user
+          setTokenHelper(action.payload.token)
+          state.user.status = '로그인 성공!'
+        } else {
+          state.user = action.payload.user
+          state.user.status = '로그인 실패!'
+        }
+      })
+      .addCase(loginUser.rejected, (state) => {
+        state.status = 'failed'
+      })
+  },
+})
+
+export default userSlice
