caa08fc626fecbfdb3a5dcdf441c342ac3aff40c
motojate <motojate@naver.com>
2023-09-06 18:44:06 +0900

    * 회원가입 관련 api 작성

--- client/src/apis/signUpUser.ts
+++ client/src/apis/signUpUser.ts
@@ -1,1 +1,10 @@
 import axios from 'axios'
+
+export const signUpUserApi = () => {
+  try {
+    const response = axios.get('api/sign-up')
+    return response
+  } catch (e) {
+    console.error(e)
+  }
+}
