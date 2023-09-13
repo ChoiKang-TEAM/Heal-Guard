2ee3f9957d3359d44defb0c9755632f0d661cab1
motojate <motojate@naver.com>
2023-09-13 23:39:03 +0900

    * any 타입 변경

--- client/src/hooks/useEmailVerification.ts
+++ client/src/hooks/useEmailVerification.ts
@@ -1,1 +1,36 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
+
+const useEmailVerification = () => {
+  const [authCode, setAuthCode] = useState('')
+  const [inputCode, setInputCode] = useState('')
+  const [isVerified, setIsVerified] = useState(false)
+
+  const fetchAuthCode = useCallback(async () => {
+    const fetchedCode = '111111'
+    setAuthCode(fetchedCode)
+  }, [])
+
+  const handleInputChange = (e: InputEvent) => {
+    console.log(e)
+    setInputCode('e')
+  }
+
+  const handleVerify = () => {
+    if (authCode === inputCode) {
+      setIsVerified(true)
+    } else {
+      setIsVerified(false)
+      alert('Invalid code. Please try again.')
+    }
+  }
+
+  return {
+    fetchAuthCode,
+    handleInputChange,
+    handleVerify,
+    isVerified,
+    inputCode,
+  }
+}
+
+export default useEmailVerification
