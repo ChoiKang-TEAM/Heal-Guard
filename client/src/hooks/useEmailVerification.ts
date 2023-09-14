a8b1a62e22aac1e46e86fd583a825e894cbb94d2
motojate <motojate@naver.com>
2023-09-14 17:39:35 +0900

    * 커스텀 훅 상태 변경

--- client/src/hooks/useEmailVerification.ts
+++ client/src/hooks/useEmailVerification.ts
@@ -1,35 +1,13 @@
-import { useState, useCallback } from 'react'
+import { useState } from 'react'
 
 const useEmailVerification = () => {
-  const [authCode, setAuthCode] = useState('')
-  const [inputCode, setInputCode] = useState('')
-  const [isVerified, setIsVerified] = useState(false)
-
-  const fetchAuthCode = useCallback(async () => {
-    const fetchedCode = '111111'
-    setAuthCode(fetchedCode)
-  }, [])
-
-  const handleInputChange = (e: InputEvent) => {
-    console.log(e)
-    setInputCode('e')
-  }
-
-  const handleVerify = () => {
-    if (authCode === inputCode) {
-      setIsVerified(true)
-    } else {
-      setIsVerified(false)
-      alert('Invalid code. Please try again.')
-    }
-  }
+  const [fields, setFields] = useState({
+    verifyCode: null,
+  })
 
   return {
-    fetchAuthCode,
-    handleInputChange,
-    handleVerify,
-    isVerified,
-    inputCode,
+    fields,
+    setFields,
   }
 }
 
