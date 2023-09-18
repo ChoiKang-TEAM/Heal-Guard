74756d1bf298000cd0ac61b10da075009e7cfc6e
motojate <motojate@naver.com>
2023-09-18 18:54:11 +0900

    * 이메일 인증 관련 api 로직 변경 -> 새로 추가된 타입으로 리턴값 변경

--- client/src/apis/verificationUser.ts
+++ client/src/apis/verificationUser.ts
@@ -1,21 +1,17 @@
 import { AuthUserVerifyCodeByEmailInput } from 'src/types/interface/sign-up/signUpInterface'
 import getUerApi from 'src/apis/getUser'
-import {
-  ID_IN_USE_MESSAGE,
-  VERIFICATION_ERROR_MESSAGE,
-} from 'src/common/constants/messages'
-import { VerificationResult } from 'src/types/type/signUpType'
+import { VerificationResult } from 'src/types/interface/states'
 
 const getAuthVerifyCodeByEmail = async (
   dto: AuthUserVerifyCodeByEmailInput
 ): Promise<VerificationResult> => {
   try {
     const isUsed = await getUerApi.isUsedUserId(dto.userId)
-    if (isUsed) return { error: ID_IN_USE_MESSAGE }
-    else return { code: '111111' }
+    if (isUsed) return { verificationState: 'InUse' }
+    else return { verifyCode: '111111', verificationState: 'Created' }
   } catch (e) {
     console.error('Error while checking user Email: ', e)
-    return { error: VERIFICATION_ERROR_MESSAGE }
+    return { verificationState: 'Error' }
   }
 }
 
--- client/src/types/type/states.ts
+++ client/src/types/type/states.ts
@@ -2,6 +2,7 @@ export type VerificationStateType =
   | 'Idle'
   | 'Created'
   | 'Expired'
+  | 'InUse'
   | 'Authentication'
   | 'Mismatched'
   | 'Error'
