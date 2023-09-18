0a81fa51e82e1c254e0b7e573ef43e1eca6e6d8c
motojate <motojate@naver.com>
2023-09-18 18:52:25 +0900

    * 이메일 인증 관련 커스텀 훅 수정

--- client/src/hooks/useEmailVerification.ts
+++ client/src/hooks/useEmailVerification.ts
@@ -1,13 +1,47 @@
 import { useState } from 'react'
+import verificationUserApi from 'src/apis/verificationUser'
+import { AuthUserVerifyCodeByEmailInput } from 'src/types/interface/sign-up/signUpInterface'
+import { VerificationState } from 'src/types/interface/states'
 
 const useEmailVerification = () => {
-  const [fields, setFields] = useState({
+  const [field, setField] = useState({
     verifyCode: null,
+    message: null,
+    state: 'idle',
+    userId: null,
   })
+  const [inputCode, setInputCode] = useState<string>('')
+
+  const handleFieldChange = (data: VerificationState) => {
+    console.log(field)
+    console.log(data)
+  }
+
+  const createVerifyCode = async (userId: string) => {
+    const dto: AuthUserVerifyCodeByEmailInput = { userId }
+    const createVerifyCodeData =
+      await verificationUserApi.getAuthVerifyCodeByEmail(dto)
+
+    const data: VerificationState = {
+      verifyCode: createVerifyCodeData ?? '',
+    }
+    console.log(data)
+  }
+
+  const state = {
+    inputCode,
+  }
+
+  const action = {
+    setField,
+    setInputCode,
+    handleFieldChange,
+    createVerifyCode,
+  }
 
   return {
-    fields,
-    setFields,
+    ...state,
+    ...action,
   }
 }
 
