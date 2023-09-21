180f6d05eac602f2769c01dea13b223c3744eaa9
motojate <motojate@naver.com>
2023-09-21 23:43:54 +0900

    * eslint 설정 변경

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -26,5 +26,8 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint', 'react'],
-  rules: {},
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn'],
+  },
 }
