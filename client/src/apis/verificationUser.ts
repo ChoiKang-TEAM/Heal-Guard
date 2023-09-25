b817462770248d606528f46ba55347cb5ed16dbd
motojate <motojate@naver.com>
2023-09-25 23:03:21 +0900

    * response 추가

--- client/src/apis/verificationUser.ts
+++ client/src/apis/verificationUser.ts
@@ -1,14 +1,20 @@
 import { AuthUserVerifyCodeByEmailInput } from 'src/types/interface/sign-up/signUpInterface'
 import getUerApi from 'src/apis/getUser'
 import { VerificationResult } from 'src/types/interface/states'
+import axios from 'axios'
 
 const getAuthVerifyCodeByEmail = async (
   dto: AuthUserVerifyCodeByEmailInput
 ): Promise<VerificationResult> => {
   try {
     const isUsed = await getUerApi.isUsedUserId(dto.userId)
+    const response = await axios.get('/api/create/code')
     if (isUsed) return { verificationState: 'InUse' }
-    else return { verificationState: 'Created' }
+    else
+      return {
+        verificationState: 'Created',
+        expiredTime: response.data.expiredTime,
+      }
   } catch (e) {
     console.error('네트워크 통신 오류', e)
     return { verificationState: 'Error' }
