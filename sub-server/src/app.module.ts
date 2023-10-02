649726edd3be17d4be1a3d1506aa39ca052ca139
motojate <motojate@naver.com>
2023-10-02 14:54:21 +0900

    * 스케줄러 기본 셋팅 완료

--- sub-server/src/app.module.ts
+++ sub-server/src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { PrismaModule } from './shared/prisma/prisma.module'
 import { AuthModule } from './member/auth/auth.module'
+import { ScheduleModule } from '@nestjs/schedule'
+import { TasksService } from './shared/services/tasks.service'
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, AuthModule, ScheduleModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService, TasksService]
 })
 export class AppModule {}
