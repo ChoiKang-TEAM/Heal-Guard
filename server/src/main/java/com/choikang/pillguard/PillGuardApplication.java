5c37a5acf052be87c787e8f644b58b6e8292a5ba
jy3026 <jy_3026@naver.com>
2023-10-02 14:49:56 +0900

    Init: 백엔드 초기 셋팅

---
+++ server/src/main/java/com/choikang/pillguard/PillGuardApplication.java
@@ -0,0 +1,13 @@
+package com.choikang.pillguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PillGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PillGuardApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/choikang/pillguard/PillGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choikang.pillguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PillGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
