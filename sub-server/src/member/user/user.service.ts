eec54e8b2dc9a4403bc751980ab1b80c3377d31d
motojate <motojate@naver.com>
2023-10-05 20:41:12 +0900

    * jwt 관련 설정

--- sub-server/src/member/user/user.service.ts
+++ sub-server/src/member/user/user.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common'
 import { User } from '@prisma/client'
 import { CrudService } from 'src/shared/interfaces/factory.interface'
+import { PrismaService } from 'src/shared/prisma/prisma.service'
 
 @Injectable()
 export class UserService implements CrudService<User> {
+  constructor(private readonly prisma: PrismaService) {}
+
   create(dto: Partial<{ id: number; userId: string; password: string; name: string; userSeq: string; createdAt: Date; updatedAt: Date; age: number }>): Promise<boolean> {
     throw new Error('Method not implemented.')
   }
@@ -13,10 +16,15 @@ export class UserService implements CrudService<User> {
   update(dto: Partial<{ id: number; userId: string; password: string; name: string; userSeq: string; createdAt: Date; updatedAt: Date; age: number }>): Promise<boolean> {
     throw new Error('Method not implemented.')
   }
-  findUnique(
-    dto: Partial<{ id: number; userId: string; password: string; name: string; userSeq: string; createdAt: Date; updatedAt: Date; age: number }>
-  ): Promise<{ id: number; userId: string; password: string; name: string; userSeq: string; createdAt: Date; updatedAt: Date; age: number }> {
-    throw new Error('Method not implemented.')
+  async findUnique(dto: { userId: string }): Promise<User> {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: {
+          userId: dto.userId
+        }
+      })
+      return user
+    } catch (e) {}
   }
   findAll(): Promise<{ id: number; userId: string; password: string; name: string; userSeq: string; createdAt: Date; updatedAt: Date; age: number }[]> {
     throw new Error('Method not implemented.')
