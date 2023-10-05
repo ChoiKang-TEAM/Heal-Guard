15f56b7d33b75a68f85002e1c611fdc6a0cdf3a7
jy3026 <jy_3026@naver.com>
2023-10-05 23:12:30 +0900

    feat: 외부 JWT토큰 검증 구현

--- server/src/main/java/com/choikang/healguard/auth/config/SecurityConfiguration.java
+++ server/src/main/java/com/choikang/healguard/auth/config/SecurityConfiguration.java
@@ -1,11 +1,16 @@
 package com.choikang.healguard.auth.config;
 
+import com.choikang.healguard.auth.filter.JwtVerificationFilter;
+import com.choikang.healguard.auth.jwt.JwtTokenizer;
+import com.choikang.healguard.auth.utils.CustomAuthorityUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -15,7 +20,13 @@ import java.util.Arrays;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+    private final JwtTokenizer jwtTokenizer;
+    private final CustomAuthorityUtils authorityUtils;
 
+    public SecurityConfiguration(JwtTokenizer jwtTokenizer, CustomAuthorityUtils authorityUtils) {
+        this.jwtTokenizer = jwtTokenizer;
+        this.authorityUtils = authorityUtils;
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
@@ -28,7 +39,7 @@ public class SecurityConfiguration {
 
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-        http.csrf().disable().formLogin().disable().httpBasic().disable();
+        http.csrf().disable().formLogin().disable().httpBasic().disable().apply(new CustomFilterConfigurer());
 
         return http.build();
     }
@@ -46,5 +57,15 @@ public class SecurityConfiguration {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfiguration);
         return source;
+    }
+
+    public class CustomFilterConfigurer extends AbstractHttpConfigurer<CustomFilterConfigurer, HttpSecurity> {
+        @Override
+        public void configure(HttpSecurity builder) throws Exception {
+            JwtVerificationFilter jwtVerificationFilter = new JwtVerificationFilter(jwtTokenizer,authorityUtils);
+
+            builder
+                    .addFilterAfter(jwtVerificationFilter, UsernamePasswordAuthenticationFilter.class);
+        }
     }
 }
