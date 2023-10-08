7ed7ff9064a43d7082c46248db252c5a9e694ac9
jy3026 <jy_3026@naver.com>
2023-10-08 23:39:33 +0900

    feat: 운동 status 값 및 create 설계 [WT]: back-end [RV]: motojate - 운동 status 값 설계 - create 설계

--- server/src/main/java/com/choikang/healguard/exercise/dto/CreateExerciseReqDto.java
+++ server/src/main/java/com/choikang/healguard/exercise/dto/CreateExerciseReqDto.java
@@ -1,4 +1,12 @@
 package com.choikang.healguard.exercise.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CreateExerciseReqDto {
+    private String name;
+    private String category;
+    private String description;
 }
