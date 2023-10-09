556767ad7afa9147e75897a02de17e6572fa81b9
jy3026 <jy_3026@naver.com>
2023-10-09 22:37:32 +0900

    ✨ [Feat] 운동 get 설계, 페이지네이션 적용 [WT]: back-end [RV]: motojate - 운동 get 설계 - 페이지네이션 적용

--- server/src/main/java/com/choikang/healguard/exercise/service/ExerciseService.java
+++ server/src/main/java/com/choikang/healguard/exercise/service/ExerciseService.java
@@ -3,6 +3,9 @@ package com.choikang.healguard.exercise.service;
 import com.choikang.healguard.exercise.entity.Exercise;
 import com.choikang.healguard.exercise.repository.ExerciseRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -17,4 +20,11 @@ public class ExerciseService {
         exerciseRepository.save(exercise);
     }
 
+    public Page<Exercise> findExercises(int page,int size) {
+        return exerciseRepository.findAll(PageRequest.of(page - 1, size, Sort.by(Sort.Direction.DESC, "id")));
+    }
+
+    public Exercise findExercise(long exerciseId) {
+        return exerciseRepository.findById(exerciseId).orElseThrow();
+    }
 }
