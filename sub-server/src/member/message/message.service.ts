036358a981477027b930ac223c83a19f6946cceb
motojate <motojate@naver.com>
2023-10-15 23:18:52 +0900

    ✨ [Feat] email-verification [WT]: back-end [RV]: jy3026 - 이메일 전송 로직을 따로 message-service로 분리

--- sub-server/src/member/message/message.service.ts
+++ sub-server/src/member/message/message.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common'
+import * as nodeMailer from 'nodemailer'
 
 @Injectable()
-export class MessageService {}
+export class MessageService {
+  async sendMail(to: string, subject: string, html: string): Promise<void> {
+    const transport = nodeMailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.AUTH_MAIL_USERID,
+        pass: process.env.AUTH_MAIL_PASSWORD
+      }
+    })
+
+    const mailOptions = {
+      to,
+      subject,
+      html
+    }
+
+    await transport.sendMail(mailOptions)
+  }
+}
