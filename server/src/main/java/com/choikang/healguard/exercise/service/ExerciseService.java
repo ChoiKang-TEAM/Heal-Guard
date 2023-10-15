c0873b5340b36b77e52a3c1903084eca7bf9ee41
jy3026 <jy_3026@naver.com>
2023-10-15 21:55:59 +0900

    ✨ [Feat] 운동 Update 구현 [WT]: back-end [RV]: motojate - 운동 Update 구현

--- server/src/main/java/com/choikang/healguard/exercise/service/ExerciseService.java
+++ server/src/main/java/com/choikang/healguard/exercise/service/ExerciseService.java
@@ -1,5 +1,6 @@
 package com.choikang.healguard.exercise.service;
 
+import com.choikang.healguard.exercise.dto.UpdateExerciseReqDto;
 import com.choikang.healguard.exercise.entity.Exercise;
 import com.choikang.healguard.exercise.repository.ExerciseRepository;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Transactional
 @RequiredArgsConstructor
@@ -30,5 +32,16 @@ public class ExerciseService {
 
     public void deleteExercise(long exerciseId) {
         exerciseRepository.deleteById(exerciseId);
+    }
+
+    public Exercise updateExercise(Exercise exercise) {
+        Optional<Exercise> optionalExercise = exerciseRepository.findById(exercise.getId());
+        Exercise findExercise = optionalExercise.orElseThrow();
+
+        Optional.ofNullable(exercise.getName()).ifPresent(findExercise::setName);
+        Optional.ofNullable(exercise.getCategory()).ifPresent(findExercise::setCategory);
+        Optional.ofNullable(exercise.getDescription()).ifPresent(findExercise::setDescription);
+
+        return exerciseRepository.save(findExercise);
     }
 }
