1976d521f93ed74e868c613e9e025aa5fa3ccb9b
jy3026 <jy_3026@naver.com>
2023-10-24 20:45:48 +0900

    ✨ [Fix] 컨벤션 수정 [WT]: back-end [RV]: motojate - 컨벤션 수정

--- server/src/main/java/com/choikang/healguard/user/controller/UserController.java
+++ server/src/main/java/com/choikang/healguard/user/controller/UserController.java
@@ -1,14 +1,11 @@
 package com.choikang.healguard.user.controller;
 
 import com.choikang.healguard.user.dto.CreateUserReqDto;
-import com.choikang.healguard.user.dto.CreateUserRespDto;
+import com.choikang.healguard.user.dto.UserRespDto;
 import com.choikang.healguard.user.entity.User;
 import com.choikang.healguard.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -16,9 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
 
-    @PostMapping("/user/create")
-    public CreateUserRespDto createUser(@RequestBody CreateUserReqDto request) {
+    @PostMapping("/users/create")
+    public UserRespDto createUser(@RequestBody CreateUserReqDto request) {
         userService.createUser(new User(request));
-        return new CreateUserRespDto();
+        return new UserRespDto();
     }
 }
