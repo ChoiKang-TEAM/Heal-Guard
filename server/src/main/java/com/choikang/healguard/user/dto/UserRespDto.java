431a2217cfd91313a975766e1d66083b1fe8f023
jy3026 <jy_3026@naver.com>
2023-10-28 23:44:11 +0900

    ✨ [Fix] 유저 응답 Dto 수정 [WT]: back-end [RV]: motojate - result 값 추가

--- server/src/main/java/com/choikang/healguard/user/dto/UserRespDto.java
+++ server/src/main/java/com/choikang/healguard/user/dto/UserRespDto.java
@@ -1,11 +1,22 @@
 package com.choikang.healguard.user.dto;
 
 import com.choikang.healguard.common.consts.ResultCode;
+import com.choikang.healguard.user.entity.User;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserRespDto {
     private int code = ResultCode.SUCCESS.value();
+
+    private User result;
+
+    public UserRespDto(User result) {
+        this.result = result;
+    }
 }
