b9ee14d2d8cd38e9bba099068e62cb0801ee24e5
motojate <motojate@naver.com>
2023-11-08 23:40:35 +0900

    ✨ [Feat] validate-global-pipe [WT]: back-end [RV]: jy3026 - 에러 코드 변수로 모두 수정

--- sub-server/src/shared/utils/response.util.ts
+++ sub-server/src/shared/utils/response.util.ts
@@ -1,5 +1,5 @@
 type ResponseCodeType = 'SUCCESS' | 'EMPTY_DATA'
-type ErrorCodeType = 'INVALID_USER' | 'BAD_REQUEST'
+type ErrorCodeType = 'INVALID_USER' | 'BAD_REQUEST' | 'IS_USED_CASE' | 'INVALID_CREDENTIALS'
 
 export const RESPONSE_CODES: { [key in ResponseCodeType]: number } = {
   SUCCESS: 1000,
@@ -7,6 +7,9 @@ export const RESPONSE_CODES: { [key in ResponseCodeType]: number } = {
 }
 
 export const ERROR_CODES: { [key in ErrorCodeType]: number } = {
+  BAD_REQUEST: 2000,
+
+  IS_USED_CASE: 3000,
   INVALID_USER: 3001,
-  BAD_REQUEST: 2000
+  INVALID_CREDENTIALS: 3002
 }
